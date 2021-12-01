4f5537fabfda333c49fffc39d0a7019d690835a4
Nice <nicenicegame@gmail.com>
2021-12-01 20:48:11 +0700

    Add cors

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config()
-// }
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const express = require('express')
-// const mongoose = require('mongoose')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
-
-// mongoose.connect(process.env.MONGODB_URL)
+app.use(cors())
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB'))
 
 app.use('/api/v1', require('./api/v1'))
 
