f794397e4be249a2c4c97a6b225c7c8156295a23
Nice <nicenicegame@gmail.com>
2021-12-12 16:40:17 +0700

    remove chart page css

--- server/models/MoodData.js
+++ server/models/MoodData.js
@@ -1,8 +1,8 @@
 const { Schema, model } = require('mongoose')
 
 const MoodData = new Schema({
-    genreName: String,
-    musics: [String]
+  moodName: String,
+  musics: [String]
 })
 
 module.exports = model('MoodData', MoodData)
